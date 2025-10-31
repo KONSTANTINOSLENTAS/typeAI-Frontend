2969dca05980c6caf9db575661bc183e997aa301
KONSTANTINOSLENTAS <146835481+KONSTANTINOSLENTAS@users.noreply.github.com>
2025-10-31 22:22:54 +0200

    Confirming final path fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/typing-ai-frontend/' // <-- MUST BE YOUR REPO NAME
+  // ⬇️ THIS MUST BE THE REPOSITORY NAME ⬇️
+  base: '/typing-ai-frontend/' 
 })
