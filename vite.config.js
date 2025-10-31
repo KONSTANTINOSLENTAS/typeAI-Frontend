d7d813395adf0cf1c31a1b478ac87307ded2b3f0
KONSTANTINOSLENTAS <146835481+KONSTANTINOSLENTAS@users.noreply.github.com>
2025-10-31 22:09:37 +0200

    Final fix: Use absolute path and define homepage for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/typing-ai-frontend/' // <-- THIS MUST BE CORRECT
+  base: '/' // <-- SET TO ABSOLUTE ROOT '/'
 })
